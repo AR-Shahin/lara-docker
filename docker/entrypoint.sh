a63919d5140b06d0e7c441663d0f3ef576c7b74c
ar-shahin <rahman.anisur@sslwireless.com>
2025-05-28 17:04:52 +0600

    quick issue fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,21 +2,23 @@
 
 cd /var/www
 
+# Copy .env if not already present
 if [ ! -f ".env" ]; then
   cp .env.example .env
 fi
 
+# Install Composer dependencies
 composer install --no-interaction --prefer-dist
 
-cp .env.example .env
-
+# Laravel setup
 php artisan key:generate
 php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-chown -R www-data:www-data storage bootstrap/cache
+# Only fix permissions (no chown!) — so host user remains owner
 chmod -R 775 storage bootstrap/cache
-
+chown -R www-data:www-data /var/www
+# Run PHP FPM
 exec php-fpm
